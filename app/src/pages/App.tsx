cbc0babcb53c2ce55f06b28de23b2695a9cb63e2
Welison Almeida <welison.almeida.923@gmail.com>
2021-04-13 15:40:28 -0300

    Add collor pallets to project

--- app/src/pages/App.tsx
+++ app/src/pages/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import ThreeDRotation from '@material-ui/icons/ThreeDRotation';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { light: "#FEFAE0", main: "#DDA15E", dark: "#BC6C25" },
+    secondary: { light: "#606C38", main: "#283618" },
+    success: { main: "#4CAF50" },
+    error: { main: "#E10050" }
+  },
+});
 
 function App() {
   return (
-    <div>
-      <header>
-        Projeto ECOmposteira
-        <ThreeDRotation />
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <header>
+          <Button variant="contained" color="secondary">
+            Hello World
+          </Button>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
