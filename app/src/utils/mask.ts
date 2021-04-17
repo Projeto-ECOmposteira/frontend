98deef505312cd5eebd1184010f5515185dd4d74
Andre Pinto <andrelucax@gmail.com>
2021-04-16 22:35:04 -0300

    add cnpj mask

--- app/src/utils/mask.ts
+++ app/src/utils/mask.ts
@@ -24,6 +24,14 @@ export function mask_cnpj(name: string): string{
 
     if (new_str.length > 12){
         new_str = new_str.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{1,2}).*/, "$1.$2.$3/$4-$5");
+    } else if (new_str.length > 8){
+        new_str = new_str.replace(/^(\d{2})(\d{3})(\d{3})(\d{1,4})/, "$1.$2.$3/$4");
+    } else if (new_str.length > 5){
+        new_str = new_str.replace(/^(\d{2})(\d{3})(\d{1,3})/, "$1.$2.$3");
+    } else if (new_str.length > 2){
+        new_str = new_str.replace(/^(\d{2})(\d{1,3})/, "$1.$2");
+    } else {
+        new_str = new_str.replace(/^(\d*)/, "$1");
     }
 
     return new_str
