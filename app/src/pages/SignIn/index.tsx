8e2c8546f77a57c152fa7fb6eabc31f3186aefef
Andre Pinto <andrelucax@gmail.com>
2021-04-25 17:30:17 -0300

    Add link to password recovery in login page

--- app/src/pages/SignIn/index.tsx
+++ app/src/pages/SignIn/index.tsx
@@ -70,7 +70,7 @@ export default function SignIn() {
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
+              <Link href="esqueceu_senha" variant="body2">
                 Esqueceu a senha?
               </Link>
             </Grid>
