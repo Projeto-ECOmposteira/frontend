e1e28dfdd4db0f2e2f085088795c6e06d6154ddd
Welison Almeida <welison.almeida.923@gmail.com>
2021-05-04 23:36:59 -0300

    Add redirect / to /home after login

--- app/src/routes/InternalRoutes.tsx
+++ app/src/routes/InternalRoutes.tsx
@@ -1,10 +1,12 @@
-import { Switch, Route } from "react-router-dom";
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "../pages/Home";
 
-export default function InternalRoutes() {
+export default function  InternalRoutes() {
   return (
     <Switch>
       <Route path="/home" component={Home} />
+      <Redirect from="/" to="/home" />
     </Switch>
   );
 }
