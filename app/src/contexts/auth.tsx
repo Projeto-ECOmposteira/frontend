61b38c71c52e1adc5ad88d5cb4fabd6213774f86
Welison Almeida <welison.almeida.923@gmail.com>
2021-05-04 23:40:18 -0300

    Add signOut function

--- app/src/contexts/auth.tsx
+++ app/src/contexts/auth.tsx
@@ -3,22 +3,27 @@ import * as auth from "../services/auth";
 
 interface AuthContextData {
   signed: boolean;
-  user: object | null;
+  user: Object | null;
   signIn(): Promise<void>;
+  signOut(): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
-  const [user, setUser] = useState<object | null>(null);
+  const [user, setUser] = useState<Object | null>(null);
 
   async function signIn() {
     const response = await auth.signIn();
     setUser(response.user);
   }
 
+  function signOut() {
+    setUser(null);
+  }
+
   return (
-    <AuthContext.Provider value={{ signed: !!user, user, signIn }}>
+    <AuthContext.Provider value={{ signed: !!user, user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
