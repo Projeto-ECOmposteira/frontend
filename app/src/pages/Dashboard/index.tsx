e87fd3a59007b0a42496867ed596d41bd71a5bb1
Welison Almeida <welison.almeida.923@gmail.com>
2021-05-06 00:54:08 -0300

    Minor code improvements

--- app/src/pages/Dashboard/index.tsx
+++ app/src/pages/Dashboard/index.tsx
@@ -1,12 +1,10 @@
 import Navbar from "../../components/Navbar";
-import InternalRoutes from "../../routes/InternalRoutes";
 
 
 export default function Dashboard() {
   return (
     <>
       <Navbar />
-      <InternalRoutes />
     </>
   );
 }
