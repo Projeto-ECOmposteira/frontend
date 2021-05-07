0aca24c51880c5d5e4d8176d003fd28d64c6d2f2
Welison Almeida <welison.almeida.923@gmail.com>
2021-05-07 19:54:01 -0300

    Add section with last warnings from composters

--- app/src/pages/Warnings/index.tsx
+++ app/src/pages/Warnings/index.tsx
@@ -48,6 +48,45 @@ export default function Warnings() {
           ]}
         />
       </Box>
+      <Box className={classes.desconectedDevices}>
+        <Typography variant="h5" component="h2">
+          Últimos alertas registrados
+        </Typography>
+        <CustomMaterialTable
+          columns={[
+            { title: "Composteira", field: "name" },
+            {
+              title: "Alerta",
+              field: "surname",
+              align: "left",
+              cellStyle: { width: "50%" },
+            },
+            { title: "Dia", field: "birthYear", align: "left" },
+            { title: "Horário", field: "birthCity", align: "left" },
+          ]}
+          data={[
+            {
+              name: "Composteira 1",
+              surname: "Desconectada há mais de 30 dias",
+              birthYear: "09/03/2021",
+              birthCity: "00h00",
+            },
+            {
+              name: "Composteira 2",
+              surname: "Desconectada há 01 dia",
+              birthYear: "09/03/2021",
+              birthCity: "00h00",
+            },
+          ]}
+          actions={[
+            {
+              icon: "search",
+              tooltip: "Visualizar composteira",
+              onClick: (event, rowData: any) => console.log(rowData.name),
+            },
+          ]}
+        />
+      </Box>
     </Container>
   );
 }
--- app/src/pages/Warnings/styles.tsx
+++ app/src/pages/Warnings/styles.tsx
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   desconectedDevices: {
-    marginBottom: theme.spacing(3),
+    marginBottom: theme.spacing(6),
   },
 }));
 
