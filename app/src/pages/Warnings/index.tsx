0f4a66204f34a031b48a7bc6494fe9745249b97c
Welison Almeida <welison.almeida.923@gmail.com>
2021-05-07 19:53:28 -0300

    Add header to desconected composters

--- app/src/pages/Warnings/index.tsx
+++ app/src/pages/Warnings/index.tsx
@@ -1,43 +1,53 @@
+import { Box } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
 import CustomMaterialTable from "../../components/CustomMaterialTable";
+import { useStyles } from "./styles";
 
 export default function Warnings() {
+  const classes = useStyles();
+
   return (
     <Container component="main">
-      <CustomMaterialTable
-        columns={[
-          { title: "Composteira", field: "name" },
-          {
-            title: "Alerta",
-            field: "surname",
-            align: "left",
-            cellStyle: { width: "50%" },
-          },
-          { title: "Dia", field: "birthYear", align: "left" },
-          { title: "Horário", field: "birthCity", align: "left" },
-        ]}
-        data={[
-          {
-            name: "Composteira 1",
-            surname: "Desconectada há mais de 30 dias",
-            birthYear: "09/03/2021",
-            birthCity: "00h00",
-          },
-          {
-            name: "Composteira 2",
-            surname: "Desconectada há 01 dia",
-            birthYear: "09/03/2021",
-            birthCity: "00h00",
-          },
-        ]}
-        actions={[
-          {
-            icon: "search",
-            tooltip: "Visualizar composteira",
-            onClick: (event, rowData: any) => console.log(rowData.name),
-          },
-        ]}
-      />
+      <Box className={classes.desconectedDevices}>
+        <Typography variant="h5" component="h2">
+          Composteiras desconectadas
+        </Typography>
+        <CustomMaterialTable
+          columns={[
+            { title: "Composteira", field: "name" },
+            {
+              title: "Alerta",
+              field: "surname",
+              align: "left",
+              cellStyle: { width: "50%" },
+            },
+            { title: "Dia", field: "birthYear", align: "left" },
+            { title: "Horário", field: "birthCity", align: "left" },
+          ]}
+          data={[
+            {
+              name: "Composteira 1",
+              surname: "Desconectada há mais de 30 dias",
+              birthYear: "09/03/2021",
+              birthCity: "00h00",
+            },
+            {
+              name: "Composteira 2",
+              surname: "Desconectada há 01 dia",
+              birthYear: "09/03/2021",
+              birthCity: "00h00",
+            },
+          ]}
+          actions={[
+            {
+              icon: "search",
+              tooltip: "Visualizar composteira",
+              onClick: (event, rowData: any) => console.log(rowData.name),
+            },
+          ]}
+        />
+      </Box>
     </Container>
   );
 }
