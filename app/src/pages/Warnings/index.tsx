213824dc96fe39aba6c276ea6756ea40691727c5
Welison Almeida <welison.almeida.923@gmail.com>
2021-05-07 18:40:38 -0300

    Add report to desconected devices

--- app/src/pages/Warnings/index.tsx
+++ app/src/pages/Warnings/index.tsx
@@ -1,13 +1,55 @@
 import Container from "@material-ui/core/Container";
 import { useStyles } from "./styles";
+import MaterialTable from "material-table";
+import { tableLocalization } from "../../utils/tableSettings";
 
-// TODO
 export default function Warnings() {
-  const classes = useStyles();
-
   return (
-    <Container  component="main">
-      <h1 className={classes.header}>Composteiras Desconectadas</h1>
+    <Container component="main">
+      <MaterialTable
+        localization={{
+          ...tableLocalization
+        }}
+        options={{
+          toolbar: false,
+          search: false,
+          showTitle: false,
+          headerStyle: {
+            backgroundColor: "#FEFAE0",
+            color: "#283618",
+            fontWeight: "bold",
+            fontSize: "1rem",
+          },
+        }}
+        columns={[
+          { title: "Composteira", field: "name"},
+          { title: "Alerta", field: "surname", align: "left", cellStyle: {width: "50%"} },
+          { title: "Dia", field: "birthYear", align: "left" },
+          { title: "Horário", field: "birthCity", align: "left" },
+        ]}
+        data={[
+          {
+            name: "Composteira 1",
+            surname: "Desconectada há mais de 30 dias",
+            birthYear: "09/03/2021",
+            birthCity: "00h00",
+          },
+          {
+            name: "Composteira 2",
+            surname:
+              "Desconectada há 01 dia",
+            birthYear: "09/03/2021",
+            birthCity: "00h00",
+          },
+        ]}
+        actions={[
+          {
+            icon: "search",
+            tooltip: "Visualizar composteira",
+            onClick: (event, rowData: any) => console.log(rowData.name),
+          },
+        ]}
+      />
     </Container>
   );
 }
