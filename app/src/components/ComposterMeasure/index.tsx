425e1e32791a6b4a97d8017ab961cd82ce934dc4
Welison Almeida <welison.almeida.923@gmail.com>
2021-05-07 01:26:24 -0300

    Add component to show composter measures

---
+++ app/src/components/ComposterMeasure/index.tsx
@@ -0,0 +1,24 @@
+import Typography from "@material-ui/core/Typography";
+import { useStyles } from "./styles";
+import { Box } from "@material-ui/core";
+
+export interface ComposterMeasure {
+  name: string;
+  image: any;
+}
+
+export default function ComposterMeasure(props: ComposterMeasure) {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <img src={props.image} alt="Peso da composteira" />
+      <Typography className={classes.title} component="h5" variant="body2">
+        {props.name}
+      </Typography>
+      <Typography className={classes.measure} component="h5" variant="h6">
+        70 kg
+      </Typography>
+    </Box>
+  );
+}
